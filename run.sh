3a307ab1d53bd990041e626b7f8bccc556c1264a
Mrozowski, Kornel <kpmrozowski@gmail.com>
2021-05-10 18:00:43 +0200

    fixed CMakeLists.txt

--- run.sh
+++ run.sh
@@ -1,6 +1,2 @@
-export PATH=cmake-install/bin:$PATH
-export CMAKE_PREFIX_PATH=cmake-install:$CMAKE_PREFIX_PATH
-CXX=/usr/bin/g++-10 CC=/usr/bin/gcc-10 cmake -S . -B out/build
-CXX=/usr/bin/g++-10 CC=/usr/bin/gcc-10 make -C out/build
-# ./out/build/src/Test/MSI_Test
-# ./out/build/src/Tool/cvrp-evo/MSI_Tool_Evo src/CVRP/E-n51-k5_VERT_COORD.txt ./src/CVRP/E-n51-k5_DEMANDS.txt
+CXX=/usr/bin/g++-10 CC=/usr/bin/gcc-10 cmake -S . -B out/build_terminal
+CXX=/usr/bin/g++-10 CC=/usr/bin/gcc-10 make -C out/build_terminal -j12 && ./out/build_terminal/src/bin/carcassonne/msi_carcassonne_bin
