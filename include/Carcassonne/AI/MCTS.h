ac17075d12d7db3466ff078c82ffafedbbe1d1f9
Bednarek Mikołaj <mikolaj.bednarek@grupawp.pl>
2021-06-22 13:58:41 +0200

    added simulation type

--- include/Carcassonne/AI/MCTS.h
+++ include/Carcassonne/AI/MCTS.h
@@ -10,14 +10,19 @@ struct FullMove;
 
 namespace carcassonne::ai {
 
+enum class SimulationType {
+   Heuristic,
+   Random
+};
+
 class Tree;
 
 mb::result<FullMove> find_non_idiotic(Tree &tree, Player player);
 void simulate(Tree &tree, NodeId node_id);
-void expand(Tree &tree, NodeId node_id);
+void expand(Tree &tree, NodeId node_id, carcassonne::ai::SimulationType simulation_type);
 void backpropagate(Tree &tree, NodeId node_id, Player winner);
-void run_selection(Tree &tree);
-void run_mcts(Tree &tree, mb::i64 time_limit);
+void run_selection(Tree &tree, carcassonne::ai::SimulationType sim_type);
+void run_mcts(Tree &tree, mb::i64 time_limit, carcassonne::ai::SimulationType sim_type);
 FullMove choose_move(Tree &tree, int move_index, Player player);
 
 }// namespace carcassonne::ai
