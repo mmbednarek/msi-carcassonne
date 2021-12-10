ce3b59fe82f529351e0ab47160fa48f0155deb1f
kpmrozowski <kpmrozowski@gmail.com>
2021-12-10 05:56:49 +0100

    added forward of different vector state

--- src/Test/CaffeTest.cpp
+++ src/Test/CaffeTest.cpp
@@ -1,7 +1,6 @@
 #include <fmt/core.h>
 #include <caffe/caffe.hpp>
 #include <caffe/filler.hpp>
-#include <caffe/layers/memory_data_layer.hpp>
 #include <caffe/layers/input_layer.hpp>
 #include <caffe/net.hpp>
 #include <caffe/util/math_functions.hpp>
@@ -38,13 +37,15 @@ void test_net() {
   //   label[i] = c;
   // }
   const size_t batch_size = 7;
-  const size_t size_S = batch_size * 41 * 41 * 86 * 180;
-  const size_t size_P = batch_size * 41 * 41 * 40 * 180;
-  const size_t size_z = batch_size * 1 * 1 * 1 * 180;
+  const size_t samples_count = 180;
+  const size_t size_S = batch_size * 41 * 41 * 86 * samples_count;
+  const size_t size_P = batch_size * 41 * 41 * 40 * samples_count;
+  const size_t size_z = batch_size * 1 * 1 * 1 * samples_count;
   // std::array<float, size_S> data;
   // std::array<float, size_P> probabilities; // labels
   // std::array<float, size_z> values;
   float *data = new float[size_S];
+  float *test_data = new float[size_S];
   float *probabilities = new float[size_P];
   float *values = new float[size_z];
   memset(data, 0, size_S * sizeof(float));
@@ -53,6 +54,7 @@ void test_net() {
   for (int i = 0; i < size_z; ++i) {
     for (int j = 0; j < size_S/size_z; ++j) {
       data[j + i * size_z] = j % 2;
+      test_data[j + i * size_z] = (j+1) % 2;
     }
     for (int j = 0; j < size_P/size_z; ++j)
       probabilities[j + i * size_z] = .5;
@@ -62,7 +64,7 @@ void test_net() {
   
   if (caffe::Caffe::mode() == caffe::Caffe::CPU) printf("mode=CPU\n");
   if (caffe::Caffe::mode() == caffe::Caffe::GPU) printf("mode=GPU\n");
-  caffe::Caffe::set_mode(caffe::Caffe::GPU);
+  caffe::Caffe::set_mode(caffe::Caffe::CPU);
   if (caffe::Caffe::mode() == caffe::Caffe::CPU) printf("mode=CPU\n");
   if (caffe::Caffe::mode() == caffe::Caffe::GPU) printf("mode=GPU\n");
   caffe::Caffe::SetDevice(0);
@@ -71,24 +73,11 @@ void test_net() {
   caffe::SolverParameter solver_param;
   caffe::ReadSolverParamsFromTextFileOrDie("./solver.prototxt", &solver_param);
   fmt::print("model: {}\n", solver_param.net());
-//   std::shared_ptr<caffe::Solver<float>> solver(
-//       caffe::SolverRegistry<float>::CreateSolver(solver_param));
   caffe::SGDSolver<float>* solver = new caffe::SGDSolver<float>(solver_param);
-  
-  // caffe::MemoryDataLayer<float> *valuesLayer_trainnet =
-  //     (caffe::MemoryDataLayer<float>
-  //          *)(solver->net()->layer_by_name("Input_value").get());
-  caffe::InputLayer<float> *valuesLayer_trainnet =
-      (caffe::InputLayer<float>
-           *)(solver->net()->layer_by_name("Input_value").get());
-  // caffe::Blob<float>* input_layer = solver->net()->input_blobs()[0];
-  // caffe::Blob<float>* output_layer = solver->net()->output_blobs()[0];
-
-  // valuesLayer_trainnet->Reset(vvalues.data(), pvalues.data(), batch_size * 180);
 
   caffe::NetParameter net_parameter;
 
-  std::ifstream t("./model.prototxt");
+  std::ifstream t("./net_tic_tac_6_4_2_res_block.prototxt");
   std::string model((std::istreambuf_iterator<char>(t)),
                  std::istreambuf_iterator<char>());
   bool success = google::protobuf::TextFormat::ParseFromString(model, &net_parameter);
@@ -97,46 +86,26 @@ void test_net() {
     net_parameter.mutable_state()->set_phase(caffe::TRAIN);
     solver->net() = boost::shared_ptr<caffe::Net<float>>(new caffe::Net<float>(net_parameter));
   }
-  // solver->net()->Init(net_parameter);
   fmt::print("size={}\n", solver->net()->input_blobs().size() );
   for (int i = 0; i < solver->net()->input_blobs().size(); ++i)
     fmt::print("{}: size={}\n", i, solver->net()->input_blob_indices()[i] );
-  fmt::print("size={}\n", valuesLayer_trainnet->layer_param().IsInitialized() );
-  fmt::print("size={}\n", valuesLayer_trainnet->layer_param().blobs_size() );
-  fmt::print("size={}\n", valuesLayer_trainnet->ExactNumBottomBlobs() );
-  fmt::print("size={}\n", valuesLayer_trainnet->ExactNumTopBlobs() );
-  fmt::print("size={}\n", valuesLayer_trainnet->blobs().size() );
   
-  caffe::MemoryDataLayer<float> *dataLayer_trainnet =
-      (caffe::MemoryDataLayer<float>
-           *)(solver->net()->layer_by_name("Input_data").get());
   boost::shared_ptr<caffe::Blob<float>> input_data_blob = solver->net()->blob_by_name("input_data");
   boost::shared_ptr<caffe::Blob<float>> output_data_blob = solver->net()->blob_by_name("output_probas");
   boost::shared_ptr<caffe::Blob<float>> label_value_blob = solver->net()->blob_by_name("label_value");
 
-  // float* input_value2 = input_value_blob->mutable_cpu_data();
-  // float* output_value2 = output_value_blob->mutable_cpu_data();
-  // memcpy(input_value2, vvalues.data(), size_z * sizeof(float));
-  // memcpy(output_value2, pvalues.data(), size_z * sizeof(float));
   float* input_data_begin = input_data_blob->mutable_cpu_data();
   float* output_probas_begin = input_data_blob->mutable_cpu_data();
   float* label_value_begin = label_value_blob->mutable_cpu_data();
   if (input_data_begin == nullptr || output_probas_begin == nullptr || label_value_begin == nullptr) {
     fmt::print("empty mutable_cpu_data\n");
   }
-  // memset(input_data2, 0, (size_S/size_z*batch_size*23+1) * sizeof(float));
-  // memset(output_data2, 0, (size_P/size_z*batch_size*23+1) * sizeof(float));
-  memcpy(input_data_begin, data, size_S / size_z * batch_size * sizeof(float));
-  memcpy(output_probas_begin, probabilities, size_P / size_z * batch_size * sizeof(float));
-  memcpy(label_value_begin, values, batch_size * sizeof(float));
-  // 10119621-
-  // 232751260
-  // fmt::print("input_data_begin[{}]={}\n", size_S/size_z*batch_size-1, *(input_data_begin + size_S/size_z*batch_size-1));
-  // float* output_data2 = input_data1->shape_ = 
-  // dataLayer_trainnet->Reset(data.data(), probabilities.data(), batch_size * 180);
+  memcpy(input_data_begin, data, size_S / samples_count * sizeof(float));
+  memcpy(output_probas_begin, probabilities, size_P / samples_count * sizeof(float));
+  memcpy(label_value_begin, values, size_z / samples_count * sizeof(float));
 
   fmt::print("Training:\n");
-  for (int i = 2; i >= 0; --i) {
+  for (int i = 50; i >= 0; --i) {
     fmt::print("{} :", i);
     fflush(stdout);
     solver->Step(1);
@@ -147,31 +116,39 @@ void test_net() {
 
   fmt::print("Testing preparation\n");
   std::shared_ptr<caffe::Net<float>> testnet;
-  testnet.reset(new caffe::Net<float>("./model.prototxt", caffe::TEST));
+  testnet.reset(new caffe::Net<float>("./net_tic_tac_6_4_2_res_block.prototxt", caffe::TEST));
   testnet->CopyTrainedLayersFrom("XOR_iter_50.caffemodel");
 
   fmt::print("Passing the input to it for testing\n");
-  float testab[] = {0, 0, 0, 1, 1, 0, 1, 1};
-  float testc[] = {0, 1, 1, 0};
-  caffe::MemoryDataLayer<float> *dataLayer_testnet =
-      (caffe::MemoryDataLayer<float> *)(testnet->layer_by_name("test_inputdata")
-                                            .get());
-  dataLayer_testnet->Reset(testab, testc, 4);
+  memcpy(input_data_begin, test_data, size_S / samples_count * sizeof(float));
 
   fmt::print("Testing\n");
   testnet->Forward();
 
-  boost::shared_ptr<caffe::Blob<float>> output_layer =
-      testnet->blob_by_name("output");
-  const float *begin = output_layer->cpu_data();
-  const float *end = begin + 4;
+  boost::shared_ptr<caffe::Blob<float>> output_probabs_layer =
+      testnet->blob_by_name("output_probas");
+  const float *begin_probabs = output_probabs_layer->cpu_data();
+  const float *end_probabs = begin_probabs + size_S / samples_count / batch_size;
 
-  fmt::print("Move data to a vector\n");
-  std::vector<float> result(begin, end);
-  for (int i = 0; i < result.size(); ++i) {
-    printf("input: %d xor %d,  truth: %f result by nn: %f\n",
-           (int)testab[i * 2 + 0], (int)testab[i * 2 + 1], testc[i], result[i]);
+  fmt::print("Result probabilities\n");
+  std::vector<float> result_probabs(begin_probabs, end_probabs);
+  for (int i = 0; i < result_probabs.size(); ++i) {
+    fmt::print("{:7.2e} ", result_probabs[i]);
   }
+  fmt::print("\n");
+  boost::shared_ptr<caffe::Blob<float>> output_value_layer =
+      testnet->blob_by_name("output_value");
+  const float *begin_value = output_probabs_layer->cpu_data();
+  const float *end_value = begin_value + size_z / samples_count / batch_size;
+
+  fmt::print("Result value\n");
+  std::vector<float> result_value(begin_value, end_value);
+  for (int i = 0; i < result_value.size(); ++i) {
+    fmt::print("{:7.2e} ", result_value[i]);
+  }
+  fmt::print("\n");
+
+
   // free(data);
   // free(label);
 }
