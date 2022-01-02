686b17d0fd3a1b2e7172146f0018bf76dba8f5e1
Mrozowski, Kornel <kpmrozowski@gmail.com>
2022-01-02 17:21:30 +0100

    preparation for concurrent mcts implementation

--- src/Carcassonne_GPU/Encoder/GameState.cu
+++
@@ -1,9 +0,0 @@
-#include <Carcassonne_GPU/Encoder/GameState.h>
-
-namespace carcassonne::encoder {
-
-DEVHOST bool encode_game_state(const game::Game &game, IBoolWriter &writer) {
-   return EXIT_SUCCESS;
-}
-
-}//carcassonne::encoder
