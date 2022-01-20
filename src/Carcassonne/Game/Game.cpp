c3995c1974363e660b1d221f8304f4cf797cdbab
Mrozowski, Kornel <kpmrozowski@gmail.com>
2022-01-20 01:06:17 +0100

    game's copy constructor

--- src/Carcassonne/Game/Game.cpp
+++ src/Carcassonne/Game/Game.cpp
@@ -384,8 +384,6 @@ void Game::notify_tour_finished(FullMove full_move) noexcept {
 
 std::unique_ptr<IGame> Game::clone() const noexcept {
    auto game = std::make_unique<Game>(*this);
-   game->m_next_move_callbacks.clear();
-   game->m_training_data.clear();
    return game;
 }
 
